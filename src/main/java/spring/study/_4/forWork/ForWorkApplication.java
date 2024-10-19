9beee8a1b24ac4c466381e87efa3b8724d9d93f8
이찬호 <zack6353@cowave.kr>
2024-10-19 16:02:30 +0900

    first commit

---
+++ src/main/java/spring/study/_4/forWork/ForWorkApplication.java
@@ -0,0 +1,13 @@
+package spring.study._4.forWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForWorkApplication.class, args);
+	}
+
+}
